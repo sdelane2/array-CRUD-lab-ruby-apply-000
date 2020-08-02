ffdacb857f53dcde896e0ca29bdd9350355373fa
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 21:11:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,8 +27,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  numbers = ["1", "2", "3"]
-  numbers[1]
+  numbers = ["am", "2", "3"]
+  numbers[0]
 end
 
 def retrieve_first_element_from_array(array)
