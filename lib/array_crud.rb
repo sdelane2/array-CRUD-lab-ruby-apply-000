d50ae4be63255768733847d14fd5420d2e5e6385
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 21:01:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  numbers = ["1", "2", "3"]
-  numbers << "4"
+  numbers = ["1", "2", "3", "4"]
+  numbers << "5"
 end
 
 def add_element_to_start_of_array(array, element)
