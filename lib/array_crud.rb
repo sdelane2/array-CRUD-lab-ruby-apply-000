46da1d6a5d2d2bf2dd379af1d66aa66673f64038
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 21:00:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  numbers = ["1", "2", "3"]
-  1 = numbers.shift
+  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+  lil_bub = famous_cats.shift
 end
 
 def retrieve_element_from_index(array, index_number)
