65ca2249c3440013f9d7c1c2c13f9dc6dbfaa873
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 21:04:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  famous_cats = ["lil' bub", "grumpy cat", "Maru"]
-  maru_cat = famous_cats.pop
+  famous_cats = ["lil' bub", "grumpy cat", "arrays!"]
+  arrays! = famous_cats.pop
 end
 
 def remove_element_from_start_of_array(array)
